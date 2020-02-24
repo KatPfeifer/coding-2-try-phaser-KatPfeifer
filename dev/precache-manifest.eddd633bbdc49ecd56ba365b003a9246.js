7c307f01b978532ed652df7510025c85a9a66631
Katarina <katarina@udel.edu>
2020-02-24 11:12:35 -0500

    Updates

---
+++ dev/precache-manifest.eddd633bbdc49ecd56ba365b003a9246.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3808e768fbdd0f9a7c5e450380c446b8",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/font/font.xml"
+  },
+  {
+    "revision": "72bd715c5f5b749b97b009226dcb800a",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/images/background.png"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/images/background2.png"
+  },
+  {
+    "revision": "2279af78414d2000f96531339a929c76",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/images/bee.png"
+  },
+  {
+    "revision": "3219fdadc471233be3e89acd0d1a05af",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/images/bone.png"
+  },
+  {
+    "revision": "a87d567e7c086a00e4c93f11a8ca96dc",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/images/chocolate.png"
+  },
+  {
+    "revision": "9cb24a74c4753e75ea0e49fad0761b45",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/images/dog.png"
+  },
+  {
+    "revision": "0ff88c3072eb95face880a8b7479d9d8",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/images/dogleft.png"
+  },
+  {
+    "revision": "e98cade8881032fc4236e51482bb3d82",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/images/tennisball.jpg"
+  },
+  {
+    "revision": "b1254096b73de0f14624b20e2c434cbe",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/images/tennisball.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/spritesheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-KatPfeifer/devassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "2dfef55f124e033f188d",
+    "url": "/coding-2-try-phaser-KatPfeifer/devgen.main.b913e69b19af88a3db79.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-KatPfeifer/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "d3947537d1d136ffdd58683a65fbf2c2",
+    "url": "/coding-2-try-phaser-KatPfeifer/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-KatPfeifer/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-KatPfeifer/devsw.js"
+  }
+]);
---
+++ precache-manifest.8e8db2af95e9530c96657e3267ea6a7d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3808e768fbdd0f9a7c5e450380c446b8",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "assets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "assets/font/font.xml"
+  },
+  {
+    "revision": "72bd715c5f5b749b97b009226dcb800a",
+    "url": "assets/images/background.png"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "assets/images/background2.png"
+  },
+  {
+    "revision": "2279af78414d2000f96531339a929c76",
+    "url": "assets/images/bee.png"
+  },
+  {
+    "revision": "3219fdadc471233be3e89acd0d1a05af",
+    "url": "assets/images/bone.png"
+  },
+  {
+    "revision": "a87d567e7c086a00e4c93f11a8ca96dc",
+    "url": "assets/images/chocolate.png"
+  },
+  {
+    "revision": "9cb24a74c4753e75ea0e49fad0761b45",
+    "url": "assets/images/dog.png"
+  },
+  {
+    "revision": "0ff88c3072eb95face880a8b7479d9d8",
+    "url": "assets/images/dogleft.png"
+  },
+  {
+    "revision": "e98cade8881032fc4236e51482bb3d82",
+    "url": "assets/images/tennisball.jpg"
+  },
+  {
+    "revision": "b1254096b73de0f14624b20e2c434cbe",
+    "url": "assets/images/tennisball.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "assets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "assets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "assets/spritesheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "assets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "assets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "assets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "assets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "17554bffc01ee0043be077140db2466b",
+    "url": "index.html"
+  },
+  {
+    "revision": "50c6b9bd8b1ffed9be6c",
+    "url": "main.bb2ff22ce9278b702c31.bundle.js"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "sw.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "vendors.a047ee0740c7d21714d9.bundle.js"
+  }
+]);
